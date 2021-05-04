a7eef140e913727efc9d48dd6e2b8beb94b400ef
hajix <sergioajini@gmail.com>
2021-05-03 21:19:18 -0400

    add duration checking service

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import json
 
 from fastapi import FastAPI, File, UploadFile
 
-from .utils import indexer
+from .utils import similarity_service, get_speaker_duration
 
 with open('/app/resources/config.json') as f:
     config = json.load(f)
@@ -12,20 +12,39 @@ app = FastAPI()
 
 
 @app.post("/similarity_service")
-async def create_upload_file(
+async def similarity_service_(
         wav_content: UploadFile = File(...),
         embedding: str = ''
 ):
     wav_content = await wav_content.read()
     embedding = embedding.encode()
-    result = indexer(
-        wav_content,
-        embedding,
+    result = similarity_service(
+        wav_content=wav_content,
+        embedding=embedding,
         speaker_index_path=config['speaker_index_path'],
         speaker_database_path=config['speaker_database_path'],
         wav_base_folder=config['wav_base_folder'],
         embedding_size=config['embedding_size'],
-        speaker_duration=config['speaker_duration'],
+        max_speaker_duration=config['max_speaker_duration'],
         similarity_threshold=config['similarity_threshold']
 )
     return result
+
+
+@app.post("/speaker_duration")
+async def speaker_duration_(
+        embedding: str = '',
+        similarity_threshold: float = -10.
+):
+    embedding = embedding.encode()
+    if similarity_threshold < -1:
+        similarity_threshold = config['similarity_threshold']
+    duration = get_speaker_duration(
+        embedding=embedding,
+        speaker_index_path=config['speaker_index_path'],
+        speaker_database_path=config['speaker_database_path'],
+        embedding_size=config['embedding_size'],
+        max_speaker_duration=config['max_speaker_duration'],
+        similarity_threshold=similarity_threshold
+)
+    return {'embedding': embedding, 'duration': duration}
--- app/utils.py
+++ app/utils.py
@@ -10,20 +10,25 @@ from .sqlite_interface import SingletonDB
 from .faiss_interface import SingletonFaiss
 
 
-def indexer(
-        wav_content,
+def convert_to_np(embedding):
+    embedding = np.frombuffer(base64.b64decode(embedding), dtype=np.float32)
+    return embedding
+
+
+def get_speaker_duration(
         embedding,
         speaker_index_path,
         speaker_database_path,
-        wav_base_folder,
         embedding_size,
-        speaker_duration,
+        max_speaker_duration,
         similarity_threshold
 ):
-    # search faiss for similar embeddings
-    embedding = np.frombuffer(base64.b64decode(embedding), dtype=np.float32)
     singleton_faiss = SingletonFaiss(speaker_index_path, embedding_size)
-    similarities, indexes = singleton_faiss.search(embedding, speaker_duration)
+    singleton_db = SingletonDB(speaker_database_path)
+
+    embedding = convert_to_np(embedding)
+    singleton_faiss = SingletonFaiss(speaker_index_path, embedding_size)
+    similarities, indexes = singleton_faiss.search(embedding, max_speaker_duration)
 
     # filter embeddings by threshold
     indexes = [
@@ -35,8 +40,33 @@ def indexer(
     # retrieve file duration
     singleton_db = SingletonDB(speaker_database_path)
     duration = singleton_db.get_duration(indexes)
+    return duration
+
+
+def similarity_service(
+        wav_content,
+        embedding,
+        speaker_index_path,
+        speaker_database_path,
+        wav_base_folder,
+        embedding_size,
+        max_speaker_duration,
+        similarity_threshold
+):
+    # search faiss for similar embeddings
+    duration = get_speaker_duration(
+        embedding,
+        speaker_index_path,
+        speaker_database_path,
+        embedding_size,
+        max_speaker_duration,
+        similarity_threshold
+    )
+    singleton_faiss = SingletonFaiss(speaker_index_path, embedding_size)
+    singleton_db = SingletonDB(speaker_database_path)
+
     print(f'speaker duration: {duration}')
-    if duration > speaker_duration:
+    if duration > max_speaker_duration:
         return {'staus': 'enough data from this speaker'}
 
     # insert data
@@ -46,6 +76,7 @@ def indexer(
     file_handler = io.BytesIO(wav_content)
     x, sr = sf.read(file_handler)
     duration = x.shape[0] / sr
+    embedding = convert_to_np(embedding)
     singleton_faiss.insert(embedding)
     singleton_faiss.save_index()
     singleton_db.execute_write_query(file_name, duration)
