69b36f3627b9086dd4eeb5a5ebaf6e8f3a61c037
root <root@berbidvps.4.2.2.4>
2021-05-03 21:39:02 -0400

    use fastapi default port

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install -r requirements.txt
 
 COPY app /app
 
-CMD uvicorn app.app:app --host 0.0.0.0 --port 8017
+CMD uvicorn app.app:app --host 0.0.0.0
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run -d \
     -v $(pwd)/resources:/resources \
     -v $(pwd)/wav:/wav \
-    -p 8017:8017 \
+    -p 8017:8000 \
     -e LC_ALL=C.UTF-8 \
     --name similarity-service \
     similarity-service:v1.0
